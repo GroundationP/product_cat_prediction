f4dda04f80766b4520aabe4bca750d3deac26251
GroundationP <49474662+GroundationP@users.noreply.github.com>
2025-02-15 12:12:13 +0100

    Add files via upload

---
+++ src/mongo_img_ingest.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from gridfs import GridFS
+import pickle 
+
+client = MongoClient(host="localhost", port=27017, username="datascientest", password="dst123")
+db = client["rakuten_db"]
+fs = GridFS(db)
+
+# importing dictionnary
+with open('/Users/terence/A_NOTEBOOKS/Datasciencetest/PROJET_RAKUTEN/project/data/original/rak_txt_img_dict.pkl', 'rb') as f:
+    rak_txt_img_dict = pickle.load(f)
+
+# ingesting data into Mongodb 
+for k, v in rak_txt_img_dict.items():
+    with open(f"/Users/terence/A_NOTEBOOKS/Datasciencetest/PROJET_RAKUTEN/images/image_train/{v[0]}", "rb") as r:
+        file_id = fs.put(r, key=f"{k}", imageid=f"{v[0]}", prdtypecode=f"{v[1]}", designation=f"{v[2]}", description=f"{v[3]}")        
+    #print(f"File stored with ID: {file_id}")
+
+
+
+
---
+++ src/mongo_img_retrieve.py
@@ -0,0 +1,21 @@
+import io
+from PIL import Image
+from pymongo import MongoClient
+from gridfs import GridFS
+
+client = MongoClient(host="localhost", port=27017, username="datascientest", password="dst123")
+db = client["photos"]
+fs = GridFS(db)
+
+
+file = fs.find_one({"filename": "image3.jpg"})
+img_data = file.read()
+
+# Display Image
+image = Image.open(io.BytesIO(img_data))
+image.show()
+
+
+
+
+
