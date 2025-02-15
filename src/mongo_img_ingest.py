11945eac801698b23f2e5c761a24c5aa8fa6ae63
GroundationP <49474662+GroundationP@users.noreply.github.com>
2025-02-15 12:12:49 +0100

    Update mongo_img_ingest.py

--- src/mongo_img_ingest.py
+++ src/mongo_img_ingest.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from gridfs import GridFS
 import pickle 
 
-client = MongoClient(host="localhost", port=27017, username="datascientest", password="dst123")
+client = MongoClient(host="localhost", port=27017, username="li", password="pw")
 db = client["rakuten_db"]
 fs = GridFS(db)
 
